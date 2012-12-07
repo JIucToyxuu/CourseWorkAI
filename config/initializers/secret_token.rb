6317054f8122af0a99b71fd4a4d7ab8b52446402
anton <antonduzhnov@gmail.com>
2012-12-07 13:50:33 +0400

    Happy start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseWorkAI::Application.config.secret_token = 'b3c5b37f0441af1db7ea215a09cb3cf2ecaf817ade8a2a5167513699d1908dde806e2005a3530700e5ea1a14d12107f233e775d324d2c35923b2fe86fe330f03'
